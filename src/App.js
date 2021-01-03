f7cc6dcda4a9c136ca0c9a10f3d8b7472c42c82a
Dennis Chau <34014458+d7chau@users.noreply.github.com>
2021-01-03 18:44:12 -0500

    Completed landing page basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.css";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+import background from "./img/sneakersgrayedout.png";
+import SearchBar from "material-ui-search-bar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={styles.navbar}>
+            Kickz Viewer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div
+        style={{
+          backgroundImage: "url(" + background + ")",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <SearchBar
+          onChange={() => console.log("onChange")}
+          onRequestSearch={() => console.log("onRequestSearch")}
+          style={{
+            margin: "0 auto", //first field is top and bottom, second field is left and right
+            position: "relative",
+            top: "40%",
+            padding: 10,
+            maxWidth: 1000,
+          }}
+          cancelOnEscape
+        />
+      </div>
     </div>
   );
 }
