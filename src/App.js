7a85a21d609ee2046625d85a3a4ef0d97a1749c3
Dennis Chau <34014458+d7chau@users.noreply.github.com>
2021-01-05 12:37:41 -0500

    Moved CSS elements to seperate file

--- src/App.js
+++ src/App.js
@@ -1,45 +1,27 @@
-import styles from "./App.css";
+import "./App.css";
+import React from "react";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-
-import background from "./img/sneakersgrayedout.png";
 import SearchBar from "material-ui-search-bar";
 
 function App() {
   return (
-    <div>
+    <div className="backgroundImg">
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={styles.navbar}>
+          <Typography variant="h6" className="navbar">
             Kickz Viewer
           </Typography>
         </Toolbar>
       </AppBar>
-      <div
-        style={{
-          backgroundImage: "url(" + background + ")",
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          width: "100vw",
-          height: "100vh",
-        }}
-      >
-        <SearchBar
-          onChange={() => console.log("onChange")}
-          onRequestSearch={() => console.log("onRequestSearch")}
-          style={{
-            margin: "0 auto", //first field is top and bottom, second field is left and right
-            position: "relative",
-            top: "40%",
-            padding: 10,
-            maxWidth: 1000,
-          }}
-          cancelOnEscape
-        />
-      </div>
+      <SearchBar
+        onChange={() => console.log("onChange")}
+        onRequestSearch={() => console.log("onRequestSearch")}
+        className="searchbar"
+        cancelOnEscape
+      />
     </div>
   );
 }
